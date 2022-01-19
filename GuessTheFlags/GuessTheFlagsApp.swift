f4eb36bf7b25e3de10f4f483999b9b20212579d8
Jordan Haynes <Jordancautious@gmail.com>
2022-01-19 16:09:10 -0500

    Initial Commit

---
+++ GuessTheFlags/GuessTheFlagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagsApp.swift
+//  GuessTheFlags
+//
+//  Created by Jordan Haynes on 1/19/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
