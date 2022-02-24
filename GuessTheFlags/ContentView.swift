5ccb9724161739ae127b3a0ab0c1fe330d675f7b
Jordan Haynes <Jordancautious@gmail.com>
2022-02-24 12:11:28 -0500

    Improved the code to be more readable. Also added and removed comments.

--- GuessTheFlags/ContentView.swift
+++ GuessTheFlags/ContentView.swift
@@ -12,42 +12,37 @@ struct ContentView: View {
     @State private var showingScore = false
     @State private var scoreTitle = ""
     @State private var score = 0
-    
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     
-    let title = Text("Guess The Flag!")
-    let boxTitle = Text("Tap the flag of...")
     
-    // Body of the view
+    let title = Text("Guess The Flag!")
+        .foregroundColor(.white)
+        .font(.largeTitle)
+        .bold()
+
+    let boxTitle = Text("Tap the flag of")
+        .font(.title2)
+        .bold()
+    
     var body: some View {
         ZStack {
+            // Background Gradient
             RadialGradient(stops: [
                 .init(color: Color(red:0.1, green: 0.2, blue: 0.45), location: 0.3),
                 .init(color: Color(red: 0.70, green: 0.2, blue: 0.20), location: 0.3)
             ], center: .top, startRadius: 200, endRadius: 700)
                 .ignoresSafeArea()
-
+            
             VStack {
                 Spacer()
-                
-                // Title
-                VStack {
-                    title
-                    .foregroundColor(.white)
-                }
-                .font(.largeTitle.bold())
-                
+                title
                 Spacer()
                 
-                // Box of flags
+                // Everything that's inside the box
                 VStack (spacing: 15.0) {
                     VStack {
-                        // Title of box and country
                         boxTitle
-                            .font(.subheadline)
-                            .fontWeight(.heavy)
-                            .foregroundStyle(.secondary)
                         
                         Text(countries[correctAnswer])
                             .font(.largeTitle)
@@ -70,7 +65,7 @@ struct ContentView: View {
                 .frame(maxWidth: .infinity)
                 .padding(.vertical, 20)
                 .background(.thinMaterial)
-            .clipShape(RoundedRectangle(cornerRadius: 20))
+                .clipShape(RoundedRectangle(cornerRadius: 20))
                 
                 Spacer()
                 
@@ -83,19 +78,17 @@ struct ContentView: View {
                 
                 Spacer()
                 
-                
             }
             .padding(10)
         }
-        // Alert notifcation if the user guesses correctly
+        
+        // Alert notifcation based on the user's guess
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
             Text("Your score is \(score)")
         }
     }
-
-    // Functions needed for the project
     
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
@@ -120,6 +113,6 @@ struct ContentView_Previews: PreviewProvider {
             .preferredColorScheme(.dark)
         ContentView()
             .preferredColorScheme(.light)
-
+        
     }
 }
